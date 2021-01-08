9ad0b95731eed217bfc7aab5db547c72824efece
jordyhenry <jordy.hpereira@gmail.com>
2021-01-08 18:25:44 -0300

    use patches for native UI and touch to tracking, letting script only for setting a new trakcing mode (which is not possible with patches)

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,14 @@
 const Scene = require('Scene');
-const TouchGestures = require('TouchGestures');
-const NativeUI = require('NativeUI');
-const Textures = require('Textures');
+const Patches = require('Patches');
 
 (async function(){
   const planeTracker = await Scene.root.findFirst('planeTracker')
-  const horizontalIcon = await Textures.findFirst('horizontal')
-  const verticalIcon = await Textures.findFirst('vertical')
-  const movingIcon = await Textures.findFirst('moving')
-  
-  const picker = NativeUI.picker
-  const index = 0
-  
-  const pickerConfiguration = {
-    selectedIndex : index,
-  
-    items: [
-      { image_texture: horizontalIcon },
-      { image_texture: movingIcon }
-    ]
-  }
-  
-  picker.configure(pickerConfiguration)
-  picker.visible = true
+  const selectedTrackingModeIndex = await Patches.outputs.getScalar('pickerSelectedIndex')
 
-  picker.selectedIndex.monitor().subscribe(function(index){
+  selectedTrackingModeIndex.monitor({ fireOnInitialValue: true }).subscribe((index) => {
     if(index.newValue == 0)
       planeTracker.mode = Scene.TrackingMode.PLANE
     else if(index.newValue == 1)
       planeTracker.mode = Scene.TrackingMode.MOVING_OBJECT
-  })
-  
-  TouchGestures.onTap().subscribe(function(touch){
-    planeTracker.trackPoint(touch.location)
   })
 })();
